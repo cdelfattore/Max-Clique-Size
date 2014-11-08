7e2737e0348e1b405d8f68e5c3e3ebb29f47a727
cdelfattore <cdelfattore@live.com>
2014-11-08 17:40:10 -0500

    Cross over implemented
    First cross over method implemented

--- MaxClique.java
+++ MaxClique.java
@@ -42,32 +42,79 @@ public class MaxClique {
 			System.out.println(i);
 		}*/
 
-		/*SubGraph initSubGraph = new SubGraph(randomSet());
+		/*System.out.println("Max clique of all points");
+		SubGraph initSubGraph = new SubGraph(points);
 		System.out.println(initSubGraph.points);
-		System.out.println(initSubGraph.maxCliqueArray);*/
+		System.out.println(initSubGraph.maxCliqueArray);
+		System.out.println(initSubGraph.maxCliqueSize);
+		System.out.println();*/
 		
 		//Genetic algorithm
 		//first create the inital population
-		int popSize = 12;
+		int popSize = 24;
 		ArrayList<SubGraph> popArray = new ArrayList<SubGraph>();
 		for(int i = 0; i < popSize; i++){
-			SubGraph sub = new SubGraph(randomSet());
-			//System.out.print(sub.maxCliqueSize + " ");
+			SubGraph sub = new SubGraph();
 			popArray.add(sub);
 		}
-		System.out.println();
-		popArray = sortArraySubGraph(popArray);
+		//System.out.println();
+		popArray = sortArraySubGraph(popArray,popSize);
 		
+		System.out.println("Init Pop Array");
 		for(SubGraph s : popArray){
-			System.out.print(s.maxCliqueSize + " ");
+			System.out.println(s.maxCliqueSize + " " + s.points);
 		}
 		System.out.println();
+		int generations = 10;
+		int innerLoopIterations = popArray.size() / 2;
+		for(int i = 0; i < generations; i++){
+			for(int j = 0; j < innerLoopIterations; j++){
+				//take the first two subGraphs and combine them, then the next two for the first half of popsize
+				ArrayList<Integer> child = createChildPath(popArray.get(j).maxCliqueArray, popArray.get(j+1).maxCliqueArray);
+				popArray.add(new SubGraph(child));
+				popArray = sortArraySubGraph(popArray,popSize);
 
+				
+
+			}
+			System.out.println("popArray");
+			for(SubGraph s : popArray){
+				System.out.println(s.maxCliqueSize + " " + s.points);
+			}
+			System.out.println();
+		}
+	}
+
+	//create a child path
+	public static ArrayList<Integer> createChildPath(ArrayList<Integer> pOne, ArrayList<Integer> pTwo){
+		ArrayList<Integer> childPath = new ArrayList<Integer>();
+		int smallestArraySize = pOne.size() > pTwo.size() ? pTwo.size() : pOne.size();
+		//System.out.println(smallestArraySize);
+		// int ranCrossOver = (int)(smallestArraySize * 0.25);
+		// int crossOverIndex = randomNum(ranCrossOver,smallestArraySize);
+
+		/*System.out.println(ranCrossOver);
+		System.out.println(crossOverIndex);*/
+		for(int i = 0; i < smallestArraySize;i++){
+			if(!childPath.contains( pOne.get(i) )){
+				childPath.add(pOne.get(i));	
+			}
+			if(!childPath.contains(pTwo.get(pTwo.size() - 1 - i))){
+				childPath.add(pTwo.get(pTwo.size() - 1 - i));	
+			}			
+		}
+
+		/*System.out.println("pOne " + pOne);
+		System.out.println("pTwo " + pTwo);
+		System.out.println("Child " + childPath);
+		System.out.println();*/
+
+		return childPath;
 	}
 
 	//sorting function that will simiulate the fitness function of a genetic algorithm
 	//Not any particualer sorting algorithm, its efficency is O(n^2), but stable.
-	public static ArrayList<SubGraph> sortArraySubGraph(ArrayList<SubGraph> subGraphArray){
+	public static ArrayList<SubGraph> sortArraySubGraph(ArrayList<SubGraph> subGraphArray, int popSize){
 		ArrayList<SubGraph> retSubGraphArray = new ArrayList<SubGraph>();
 		for(SubGraph s : subGraphArray){
 			//System.out.println(s.maxCliqueArray.size());
@@ -86,21 +133,31 @@ public class MaxClique {
 				for(int i = 0; i < retSubGraphArray.size(); i++){
 					if( i == 0 && s.maxCliqueSize >= retSubGraphArray.get(i).maxCliqueSize ){
 						retSubGraphArray.add(0,s);
+						break;
 					}
 					else if(i == retSubGraphArray.size()-1 && s.maxCliqueSize < retSubGraphArray.get(i).maxCliqueSize){
 						retSubGraphArray.add(retSubGraphArray.size(),s);
+						break;
 					}
 					else if(s.maxCliqueSize < retSubGraphArray.get(i).maxCliqueSize && s.maxCliqueSize >= retSubGraphArray.get(i+1).maxCliqueSize){
-						retSubGraphArray.add(i+1,s);	
+						retSubGraphArray.add(i+1,s);
+						break;
 					}
 				}
 			}
-			for(SubGraph b : retSubGraphArray){
+			/*for(SubGraph b : retSubGraphArray){
 				System.out.print(b.maxCliqueSize + " ");
 			}
-			System.out.println();
+			System.out.println();*/
 		}
-		return retSubGraphArray;
+		if(retSubGraphArray.size() >= popSize){
+			ArrayList<SubGraph> subList = new ArrayList<SubGraph>();
+			subList.addAll(retSubGraphArray.subList(0,23));
+			return subList;
+		}
+		else {
+			return retSubGraphArray;	
+		}		
 	}
 
 	public static HashSet<Integer> randomSet(){
@@ -244,18 +301,24 @@ class SubGraph {
 		CalcMaxClique();	
 	}
 
+	SubGraph(){
+		points = new ArrayList<Integer>(MaxClique.randomSet());
+		CalcMaxClique();
+	}
+
 	//function to calculate the maximum clique of a subgraph
 	void CalcMaxClique(){
 		maxCliqueArray = new ArrayList<Integer>();
 
 		if(points.size() == 1){
 			maxCliqueArray.addAll(points);
-
+			maxCliqueSize = maxCliqueArray.size();
 		}
 		else if(points.size() == 2){
 			//check if there is a edge between them
 			if( MaxClique.edgeMap.get( points.get(0) ).contains( points.get(1) ) ){
 				maxCliqueArray.addAll(points);
+				maxCliqueSize = maxCliqueArray.size();
 
 			}				
 		}
